f7009edb842501d4e90b26fa747f24fe916013cb
yxvvckzzxr <yxvvckzzxr@privaterelay.appleid.com>
2026-04-15 06:06:49 +0900

    update

---
+++ nocache_server.py
@@ -0,0 +1,12 @@
+import http.server
+import functools
+
+class NoCacheHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
+
+handler = functools.partial(NoCacheHandler, directory='/Users/suzuki/Desktop/app')
+http.server.HTTPServer(('', 8080), handler).serve_forever()
